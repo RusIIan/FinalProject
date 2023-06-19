63711d9ff442b1ee7fefd7e9feaa2eb62832d55e
Ruslan <ruslanri@code.edu.az>
2023-06-19 23:08:20 +0400

    UpdateShop

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,3 +1,36 @@
+var lowerSlider = document.querySelector("#lower");
+var upperSlider = document.querySelector("#upper");
+
+document.querySelector("#two").value = upperSlider.value;
+document.querySelector("#one").value = lowerSlider.value;
+
+var lowerVal = parseInt(lowerSlider.value);
+var upperVal = parseInt(upperSlider.value);
+
+upperSlider.oninput = function () {
+  lowerVal = parseInt(lowerSlider.value);
+  upperVal = parseInt(upperSlider.value);
+
+  if (upperVal < lowerVal + 4) {
+    lowerSlider.value = upperVal - 4;
+    if (lowerVal == lowerSlider.min) {
+      upperSlider.value = 4;
+    }
+  }
+  document.querySelector("#two").value = this.value;
+};
+
+lowerSlider.oninput = function () {
+  lowerVal = parseInt(lowerSlider.value);
+  upperVal = parseInt(upperSlider.value);
+  if (lowerVal > upperVal - 4) {
+    upperSlider.value = lowerVal + 4;
+    if (upperVal == upperSlider.max) {
+      lowerSlider.value = parseInt(upperSlider.max) - 4;
+    }
+  }
+  document.querySelector("#one").value = this.value;
+};
 (function ($) {
   $(document).ready(function () {
     $(".testimonial-slick-slider").slick({
@@ -72,27 +105,27 @@
         $(".returnCategory").addClass("disable");
       });
       // ======================================
-       $(".paymentCategory").click(function () {
-         $(".accoundCategory").removeClass("active");
-         $(".accoundCategory").addClass("disable");
-         $(".paymentCategory").removeClass("disable");
-         $(".paymentCategory").addClass("active");
-         $(".refundCategory").removeClass("active");
-         $(".refundCategory").addClass("disable");
-         $(".returnCategory").removeClass("active");
-         $(".returnCategory").addClass("disable");
-       });
+      $(".paymentCategory").click(function () {
+        $(".accoundCategory").removeClass("active");
+        $(".accoundCategory").addClass("disable");
+        $(".paymentCategory").removeClass("disable");
+        $(".paymentCategory").addClass("active");
+        $(".refundCategory").removeClass("active");
+        $(".refundCategory").addClass("disable");
+        $(".returnCategory").removeClass("active");
+        $(".returnCategory").addClass("disable");
+      });
       // ========================================
-         $(".returnCategory").click(function () {
-           $(".returnCategory").removeClass("disable");
-           $(".returnCategory").addClass("active");
-           $(".accoundCategory").removeClass("active");
-           $(".accoundCategory").addClass("disable");
-           $(".paymentCategory").removeClass("active");
-           $(".paymentCategory").addClass("disable");
-           $(".refundCategory").removeClass("active");
-           $(".refundCategory").addClass("disable");
-         });
+      $(".returnCategory").click(function () {
+        $(".returnCategory").removeClass("disable");
+        $(".returnCategory").addClass("active");
+        $(".accoundCategory").removeClass("active");
+        $(".accoundCategory").addClass("disable");
+        $(".paymentCategory").removeClass("active");
+        $(".paymentCategory").addClass("disable");
+        $(".refundCategory").removeClass("active");
+        $(".refundCategory").addClass("disable");
+      });
     });
   });
 })(jQuery);
